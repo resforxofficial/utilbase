9694d90e02c03a9aa0e8cf402fdf16efe03c28d3
unknown <139689090+resforx@users.noreply.github.com>
2024-03-05 18:42:41 +0900

    initial commit

--- reactly/src/App.tsx
+++ reactly/src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css';
+import utilizeState from 'utilbase';
 
 export default function App() {
+  const store: { args: number, update: () => void } = {
+    args: 0,
+    update: () => {}
+  }
+  const { getState, setState } = utilizeState(store);
 
   return (
     <>
+
     </>
   )
 }

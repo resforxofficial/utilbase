0edeef2a08001507b7a0dacd8602909e52203af9
unknown <139689090+resforx@users.noreply.github.com>
2024-03-05 21:11:15 +0900

    initial commit

--- reactly/src/App.tsx
+++ reactly/src/App.tsx
@@ -1,16 +1,32 @@
+/* eslint-disable */
 import './App.css';
-import utilizeState from 'utilbase';
+import utilizeState from '../../src/ts/index';
 
 export default function App() {
-  const store: { args: number, update: () => void } = {
+  type mytype = { args: number, up: () => void };
+
+  const store: mytype = {
     args: 0,
-    update: () => {}
+    up: () => {}
   }
+
   const { getState, setState } = utilizeState(store);
+  setState(v => ({
+    args: 0,
+    up: () => {
+      setState(prev => ({ ...prev, args: v.args + 1 }));
+    },
+  }));
+
+  const handleClick = () => {
+    getState().up();
+  }
 
   return (
     <>
-      
+      <h1>hello world</h1>
+      <h3>{ getState().args || 0 }</h3>
+      <button onClick={handleClick}>click</button>
     </>
   )
 }
--- src/ts/contemporary.ts
+++ src/ts/contemporary.ts
@@ -1,3 +1,28 @@
 export const sum = (a: number, b: number) : number => {
     return a + b;
 }
+
+// type StateSetter<T> = T | ((prevState: T) => T);
+
+// /**
+//  * interface State is the initial form value of return values from utilizeState
+//  * @alias getState this function gets current state
+//  * @alias setState this function sets current state to new state
+//  */
+// interface State<T> {
+//     getState: () => T;
+//     setState: (updater: StateSetter<T>) => void;
+// }
+
+// export default function utilizeState1<T>(initialState: T): State<T> {
+//     let currentState = initialState;
+
+//     const getState = () => currentState;
+
+//     const updateState = (updater: StateSetter<T>) => {
+//         const newState = typeof updater === 'function' ? (updater as (prevState: T) => T)(currentState) : updater;
+//         currentState = newState;
+//     };
+
+//     return { getState, setState: updateState };
+// }
